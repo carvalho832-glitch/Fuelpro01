39a75f5f6a577bb3001f1ef78d324b3868c6e2cc
carvalho832-glitch <carvalho832@gmail.com>
2026-05-04 21:36:18 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'fuel-pro-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// Instalação: Guarda os arquivos essenciais no celular
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Intercepta pedidos: Tenta carregar do celular primeiro, se não tiver, busca na internet
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
